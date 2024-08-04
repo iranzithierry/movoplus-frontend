d55139705c4a67f3f95fc45eb6c409498b3bdc83
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-08-04 14:22:33 +0200

    Update loading.tsx

--- app/(shopping)/marketplace/loading.tsx
+++ app/(shopping)/marketplace/loading.tsx
@@ -6,9 +6,6 @@ export default function Loading() {
             {Array.from({ length: 8 }).map((_, index) => (
                 <div key={index} className="border p-0.5 rounded-lg relative flex w-full justify-center">
                     <Skeleton className="w-full h-72 rounded-lg" />
-                    <div className="mt-4 space-y-2 absolute bottom-0 left-0 right-0 p-2">
-                        <Skeleton className="w-2/4 h-8  mx-auto rounded-full" />
-                    </div>
                     <div className='absolute z-10 top-0 right-0 flex w-full p-2 justify-end'>
                         <Skeleton className="h-8 rounded-full w-8" />
                     </div>
