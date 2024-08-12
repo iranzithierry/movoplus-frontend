ac875809a8ca0fef0e37a486f099f3eeed9f92e4
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-08-13 00:55:14 +0200

    fs

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -21,7 +21,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type === "password" && showPassword ? "text" : type}
         className={cn(
-          `flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 autofill:bg-transparent ${invalid && 'ring-red-500 focus-visible:ring-red-500'}`,
+          `flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 ${invalid && 'ring-red-500 focus-visible:ring-red-500'}`,
           className
         )}
         ref={ref}
