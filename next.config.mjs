3832f2cafc2391e186c1b9b3f70d226b33481188
Troy <iranzithierry12@gmail.com>
2025-04-10 13:50:08 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-            remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '8000',
-                pathname: '**',
-            },
-        ],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/marketplace",
+        permanent: false,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
